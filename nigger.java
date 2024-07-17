aac4f8668b2fd1c575e9e04b0c0dbf0b8359d1b7
Bogdan Kryukov <klev97814@gmail.com>
2024-07-17 14:47:30 +0200

    my first project

---
+++ nigger.java
@@ -0,0 +1,14 @@
+public class nigger {
+public static void main(String [] args) {
+   for(int  i= 0;i<=15 ;i++ ){
+    if(i%2==0){
+      continue;
+    }
+      
+
+    System.out.println("отчет окончен" + i );
+
+System.out.println("отчет окончен"+i);
+}
+}
+}
